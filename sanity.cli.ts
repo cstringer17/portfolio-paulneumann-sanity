3ebbe17e41809a9993e08385764090a130dcae79
Callum Stringer <callumstringer10@gmail.com>
2025-09-20 12:24:01 +1000

    chore: update CLI autoUpdates -> deployment.autoUpdates

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,7 @@
+// sanity.cli.ts
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {
-    projectId: '24hup4ux',
-    dataset: 'production'
-  },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  api: { projectId: '24hup4ux', dataset: 'production' },
+  deployment: { autoUpdates: true },   // ← update this
 })
